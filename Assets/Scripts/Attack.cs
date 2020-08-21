d123d5258cfb79a5ea25a288ef48f7316a531e78
bobojo <williamsokol0@gmail.com>
2020-08-21 08:24:52 -0700

    added mouse tracking

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //point wepon at mouse
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 dirction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y
+        );
+        transform.up = dirction;
+    }
+}
