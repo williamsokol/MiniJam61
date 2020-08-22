539f8445993ee46d19a75edb06e606c0ae2c9199
ludumludere-altusscholadiscipulus <38133576+ludumludere-altusscholadiscipulus@users.noreply.github.com>
2020-08-22 16:11:37 +0700

    Add files via upload

---
+++ Assets/Main Menu/ButtonClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Game Level");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
