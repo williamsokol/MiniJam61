bf127c29e3cfb50d38dcaaf93f9211280787fe9e
bobojo <williamsokol0@gmail.com>
2020-08-23 06:10:03 -0700

    added some tiles and lightinh

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    private Vector2 velocity;
+    public  GameObject  player;
+    private Vector3        offset;
+
+    
+    public void getCamera()
+    {      
+            //player = Player.instance;
+            //offset = player.transform.position - transform.position;
+            velocity = Vector2.zero;
+            print("offset");
+    }
+    void FixedUpdate()
+    {
+        if(player != null)
+            transform.position = Vector2.SmoothDamp(transform.position,player.transform.position,ref velocity,.4f);
+    }
+   
+}
