65569b772c0485ca3ccb24d5a25b559039840df1
ludumludere-altusscholadiscipulus <38133576+ludumludere-altusscholadiscipulus@users.noreply.github.com>
2020-08-24 10:57:00 +0700

    Add files via upload

---
+++ Assets/PlayerHp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHp : MonoBehaviour
+{
+    public static PlayerHp instance;
+    public GameObject player;
+
+    [SerializeField]
+    private float _hp;
+
+    //updates on hp variable being changed
+    public float hp{
+        get{return _hp;}
+
+        set{
+            _hp = value;
+            UpdateHp();
+        }
+    }
+    public void UpdateHp(){
+        // modify hp indicator
+        if(_hp <= 0)
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+
+    void Awake(){
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if(player.transform.position.y <= -20)
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+}
